cd9992c7018d3d3f547931314f655cf72c9eb7c9
zhangyz107 <zhangyz10715@163.com>
2024-12-17 11:54:22 +0800

    更新模板项目

--- template/KSW.ATE01.Template/Template/TemplateDigital.cs
+++ template/KSW.ATE01.Template/Template/TemplateDigital.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CustomerProgram.Template
+namespace CustomerProgram
 {
     public class TemplateDigital
     {
--- template/KSW.ATE01.Template/TestItems.cs
+++ template/KSW.ATE01.Template/TestItems.cs
@@ -1,15 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CustomerProgram
+﻿namespace CustomerProgram
 {
     public class TestItems
     {
-        static void Main(string[] args)
-        {
-        }
+
     }
 }
