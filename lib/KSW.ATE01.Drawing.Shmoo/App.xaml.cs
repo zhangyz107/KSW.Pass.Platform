082c6dcc103a5d35343b472f2b6ed8abdeecc9a7
zhangyz107 <zhangyz10715@163.com>
2025-03-19 18:13:28 +0800

    新增Shmoo图设置预览功能

---
+++ lib/KSW.ATE01.Drawing.Shmoo/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using KSW.ATE01.Drawing.Shmoo.Views;
+using SciChart.Charting.Visuals;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace KSW.ATE01.Drawing.Shmoo
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            SciChartSurface.SetRuntimeLicenseKey(@"SFvF1KhS4HQlwbdiPJ0lRWyTgXxnm03oAeAWkfutOztVYoWf7tCcMINFAeXT38Qf0JwxTKZIQKLYyjbHcTGuVQQSkimVS2ZYdI290ZsZhYq4OpjZ1MnSaJGzbl1Crd0DfvRt+AZRGxfbO9JtEuRfP6mO1D+NAH9DHSmrZQwjWFReopYu39eU0SWdjabaUrE44Rt1m+CrW3lUQOUzeoY/sYaL9gin4Jl+RjpGHtthhvbaD8RIHkMhHj1noSzMNb7RrdIb/emvEU4YFXZ9gfCLNOmt8EvCaBA3T7YwLl63PKWKJFMGb3+zfluDaGH+ovAoVYnU6ssQKombkvQoTSRMGpH8rYh8TdIJIMW4d0lNQhSg7F/AgQTVgyptelqEAJgQytVaR1zsgFbXVquRRTDYS7YSJG+5agzdIdlT9ACF3423I4jToSB/pVkcD7HjY5TSPN0daIFbbZ+eXvakku3l4M2+UwFolwKYTdAGwSL2jpbJuhIlVAqHYT9ie89SC7JXsZSqWFHCkqFNMnz1ww4M1b0oofOdytpI8Bu3fGEv9WQdfaMx8jw6ZFxtShQIADbGxdIDNbn+lU08sGa3P5nosHNbPIy6f0NVpDwld7PIRy4=");
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var mainWindow = new MainWindow();
+            mainWindow.ShowDialog();
+        }
+    }
+}
---
+++ lib/KSW.ATE01.Drawing.Shmoo/ViewModels/MainViewModel.cs
@@ -0,0 +1,57 @@
+﻿using KSW.ATE01.Project.Base.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KSW.ATE01.Drawing.Shmoo.ViewModels
+{
+    public class MainViewModel : ViewModelExtension
+    {
+        private ShmooViewModel _shmooViewModel = new ShmooViewModel();
+
+        public ShmooViewModel ShmooViewModel
+        {
+            get => _shmooViewModel;
+            set => SetProperty(ref _shmooViewModel, value);
+        }
+
+        public MainViewModel()
+        {
+            InitData();
+        }
+
+        private void InitData()
+        {
+            var xStart = 20.0;
+            var xEnd = 30.0;
+            var xDelta = 0.5;
+            var yStart = 3.0;
+            var yEnd = 5.5;
+            var yDelta = 0.15;
+
+            var xDistence = xEnd - xStart;
+            var rowLength = (int)Math.Round(xDistence / xDelta, 0);
+            var yDistence = yEnd - yStart;
+            var colLength = (int)Math.Round(yDistence / yDelta, 0);
+
+            var data = new bool?[rowLength, colLength];
+
+            for (var i = 0; i < rowLength; i++)
+                for (var j = 0; j < colLength; j++)
+                {
+                    if (i < rowLength / 2 || j < colLength / 2)
+                        data[i, j] = false;
+                    else if (i == rowLength / 2 || j == colLength / 2)
+                    {
+                        data[i, j] = null;
+                    }
+                    else
+                        data[i, j] = true;
+                }
+
+            ShmooViewModel.SetShmooData(20.0, 30.0, 0.5, 3, 5.5, 0.15, data, "X", "Y");
+        }
+    }
+}
